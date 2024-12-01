50f075b5992e879e855c02f06aac5322110288dd
Pavel Halukha <pavelgaluha@gmail.com>
2024-12-01 12:07:38 +0300

    add class OrientedGraph

---
+++ OrientedGraph/OrientedGraph/OrientedGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oriented_Graph
+{
+    internal class OrientedGraph<T> where T : IComparable<T>
+    {
+        public List<Vertex<T>> Verticies;
+
+        public OrientedGraph()
+        {
+            Verticies = new List<Vertex<T>>();
+        }
+
+        
+        
+    }
+}
