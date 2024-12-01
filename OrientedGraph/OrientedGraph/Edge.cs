45d596339190b2966630977fcf8e89d841e3b110
Pavel Halukha <pavelgaluha@gmail.com>
2024-12-01 12:06:42 +0300

    add classes Vertex and Edge

---
+++ OrientedGraph/OrientedGraph/Edge.cs
@@ -0,0 +1,12 @@
+﻿namespace Oriented_Graph
+{
+    internal class Edge<T>
+    {
+        public List<Vertex<T>> Vertices;
+
+        public Edge()
+        {
+            Vertices = new List<Vertex<T>>();
+        }
+    }
+}
---
+++ OrientedGraph/OrientedGraph/Vertex.cs
@@ -0,0 +1,14 @@
+﻿namespace Oriented_Graph
+{
+    internal class Vertex<T>
+    {
+        public T Value;
+        public Edge<T> Edges;
+
+        public Vertex(T value)
+        {
+            Value = value;
+            Edges = new Edge<T>();
+        }
+    }
+}
